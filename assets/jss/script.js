c9162e83306f3631ce887a57092875cf75733d11
MartinMeng1 <126767342+MartinMeng1@users.noreply.github.com>
2023-05-03 20:38:47 -0700

    completed project

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,8 +1,11 @@
+//The search button
 var searchButton = document.querySelector('#search-button');
 
+//Get the last searched-result stored in the local storage
 var lastResult= localStorage.getItem("query");
 document.querySelector("#last-result").innerHTML = lastResult;
 
+//A function that will be incorporated into the click event function below
 function handleSearchButtonSubmit(event){
     event.preventDefault();
 
@@ -21,7 +24,7 @@ function handleSearchButtonSubmit(event){
 
 
 }
-
+//When the user click the search button after typing an animal name, they will be redirect to the search-result html page with the search value.
 searchButton.addEventListener('click', handleSearchButtonSubmit);
 
 
